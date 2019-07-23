3aebfe3ed27e27fd47e320e7cfe84f48f63dde1a
hand-Sum MUSEgit <egbouchenna001@gmail.com>
2019-07-23 17:11:34 +0100

    Add Header component

---
+++ client/src/Header/Header.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import Navlink from './navLink';
+import { FaGithubAlt, FaSlack, FaTwitter } from 'react-icons/fa';
+import { NavLink } from 'react-router-dom';
+import './Header.css';
+
+export default function Header() {
+  return (
+    <div>
+      <nav id="scotchy-nav" className="navbar is-dark">
+        <div className="navbar-item">
+          <NavLink activeClassName="active" to="/home">
+            Decagon
+          </NavLink>
+        </div>
+        <ul>
+          <Navlink
+            activeClassName="active"
+            id="home"
+            to="/dashboard"
+            text="Dashboard"
+          />
+          <Navlink
+            activeClassName="active"
+            id=""
+            to="/drivers"
+            text="Drivers"
+          />
+          <Navlink
+            activeClassName="active"
+            id="github-icon"
+            to="https://www.github.com"
+            target="_blank"
+            text={<FaGithubAlt />}
+          />
+          <Navlink
+            activeClassName="active"
+            id="slack-icon"
+            to="https://www.slack.com"
+            target="_blank"
+            text={<FaSlack />}
+          />
+          <Navlink
+            activeClassName="active"
+            id="twitter-icon"
+            to="https://www.twitter.com"
+            target="_blank"
+            text={<FaTwitter />}
+          />
+        </ul>
+      </nav>
+    </div>
+  );
+}
