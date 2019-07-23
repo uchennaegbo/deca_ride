198df727eae18da1898f5623881ed52c3e244b4b
hand-Sum MUSEgit <egbouchenna001@gmail.com>
2019-07-23 17:55:47 +0100

    Add Homepage component

---
+++ client/src/Homepage/Home.js
@@ -0,0 +1,18 @@
+/* eslint-disable jsx-a11y/accessible-emoji */
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+  return (
+    <div>
+      <div className="display-img">
+        <h1>DECARIDERS TRIPSTARS✨</h1>
+        <p>
+          <em>Innovation that drives you...</em>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
