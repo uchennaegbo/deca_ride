b90379b3cad109e058eb3cdc1177536005338750
hand-Sum MUSEgit <egbouchenna001@gmail.com>
2019-07-24 20:20:04 +0100

    Import components

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, Switch } from 'react-router-dom';
+import Header from './Header/Header';
+import Home from './Homepage/Home';
+import Dashboard from './Dashboard/Dashboard';
+import Driver from './Drivers/Drivers';
+import Trip from './Trips/trips';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Switch>
+        <Route exact path="/home" component={Home} />
+        <Route exact path="/dashboard" component={Dashboard} />
+        <Route exact path="/drivers" component={Driver} />
+        <Route path="/trip" component={Trip} />
+      </Switch>
     </div>
   );
 }
